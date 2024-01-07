b9f27250965ed7d3e51850fe5a765c0d58c44e79
Sophia S. <sophia0791@icloud.com>
2024-01-07 15:37:07 +0100

    add solution

--- index.js
+++ index.js
@@ -23,13 +23,19 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length === 3);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter(
+  (card) => card.isBookmarked != true
+);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(
+  (card) =>
+    (card.tags.includes("html", "js") || card.tags.includes("js")) &&
+    card.isBookmarked == true
+);
 
 export {
   onlyCardWithIdTwo,
